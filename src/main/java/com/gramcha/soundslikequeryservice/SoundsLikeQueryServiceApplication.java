c1beb8c64d0ffc868e8417dd4e15d2bcadd1ce9f
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-10-30 02:03:03 +0530

    adding hystrix stream support

--- src/main/java/com/gramcha/soundslikequeryservice/SoundsLikeQueryServiceApplication.java
+++ src/main/java/com/gramcha/soundslikequeryservice/SoundsLikeQueryServiceApplication.java
@@ -2,12 +2,14 @@ package com.gramcha.soundslikequeryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient     // Register this service as uerka client to eureka server
 @ComponentScan(basePackages = "com.gramcha.*")
+@EnableCircuitBreaker
 public class SoundsLikeQueryServiceApplication {
 
 	public static void main(String[] args) {
